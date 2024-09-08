08e68faca220c12ec0b009b3ac10972924978fda
Hector Romero Lopez <hector@hromerol.es>
2024-09-08 21:54:23 +0200

    ADD: index.ts with .env files

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+import express from 'express';
 
+const app = express();
+app.use(express.json({strict: false}));
 
+const PORT = Number(process.env.PORT) || 3000;
+app.listen(PORT, () => {
+   console.log(`Server running`)
+});
